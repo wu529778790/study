a5c93787c30db048593b083925cd0ac03dfe5482
wu529778790 <529778790@qq.com>
2018-03-23 15:44:24 +0800

    异步请求数据，创建asyncData.vue

---
+++ nuxt/pages/asyncData.vue
@@ -0,0 +1,27 @@
+<template>
+
+    <div>
+        <h1>
+            姓名：{{ info.name }}
+        </h1>
+        <h2>年龄：{{info.age}}</h2>
+        <h2>兴趣：{{info.interest}}</h2>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      name: 'hello world'
+    };
+  },
+  async asyncData() {
+    let {data} = await axios.get('https://api.myjson.com/bins/iik7n')
+    return {
+        info: data
+    }
+  }
+};
+</script>
