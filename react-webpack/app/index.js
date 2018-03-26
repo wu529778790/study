f0433fad02243c34c871329d4c4e67825f11a435
wu529778790 <529778790@qq.com>
2018-03-26 13:37:07 +0800

    增加路由模式hash写法

--- react-webpack/app/index.js
+++ react-webpack/app/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  HashRouter,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom';
 import Shenzjd from './shenzjd';
 import Shenzjdb from './shenzjdb';
 import Shenzjdc from './shenzjdc';
@@ -8,7 +14,19 @@ import Nav from './nav';
 import Error from './404';
 
 ReactDom.render(
-  <Router>
+  // <Router>
+  //   <div>
+  //     <Nav />
+  //     <Switch>
+  //       <Route exact path="/" component={Shenzjd} />
+  //       <Route exact path="/Shenzjdb/:param" component={Shenzjdb} />
+  //       <Route exact path="/Shenzjdc" component={Shenzjdc} />
+  //       <Redirect from="/redirect" to="/Shenzjdc" />
+  //       <Route component={Error} />
+  //     </Switch>
+  //   </div>
+  // </Router>,
+  <HashRouter>
     <div>
       <Nav />
       <Switch>
@@ -19,6 +37,6 @@ ReactDom.render(
         <Route component={Error} />
       </Switch>
     </div>
-  </Router>,
+  </HashRouter>,
   document.getElementById('app')
 );
