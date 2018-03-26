a0fdc509965b4883935bf5f8a61e91dc70d97e83
wu529778790 <529778790@qq.com>
2018-03-26 14:59:17 +0800

    增加静态资源文件引入，图片等

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -11,5 +11,11 @@
         <nuxt-link :to="{name:'news',params:{newsId:3306}}">NEWS</nuxt-link>
       </li>
     </ul>
+    <div>
+      <img src="~static/logo.jpg" alt="">
+      <br/>
+      背景图片的引入
+      background-image: url('~static/logo.png')
+    </div>
   </div>
 </template>
