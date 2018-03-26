dabdd0c34552bd4ddc4889e7d945b935d3b05748
wu529778790 <529778790@qq.com>
2018-03-26 10:31:18 +0800

    增加activeClasssName样式，增加处理css的style-loader,css-loader

--- react-webpack/app/nav.js
+++ react-webpack/app/nav.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-
+import './nav.css';
 const NavBar = () => (
   <div>
     <div>
-      <NavLink exact to="/">
+      <NavLink exact to="/" className="blue">
         Shenzjd
-      </NavLink> | &nbsp;
-      <NavLink exact to="/Shenzjdb">
+      </NavLink>{' '}
+      | &nbsp;
+      <NavLink exact to="/Shenzjdb" activeClassName="active">
         Shenzjdb
-      </NavLink> | &nbsp;
-      <NavLink exact to="/Shenzjdc">
+      </NavLink>{' '}
+      | &nbsp;
+      <NavLink exact to="/Shenzjdc" activeClassName="active">
         Shenzjdc
-      </NavLink> | &nbsp;
+      </NavLink>{' '}
+      | &nbsp;
     </div>
   </div>
 );
--- react-webpack/webpack.config.js
+++ react-webpack/webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
         query: {
           presets: ['es2015', 'react']
         }
+      },
+      {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader']
       }
     ]
   }
