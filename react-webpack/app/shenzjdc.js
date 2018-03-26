23d8aa8c590dd83927658f6e33ef7a811d4eeb53
wu529778790 <529778790@qq.com>
2018-03-26 13:42:08 +0800

    增加prompt标签提示

--- react-webpack/app/shenzjdc.js
+++ react-webpack/app/shenzjdc.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Prompt } from 'react-router-dom'
 
 export default class Shenzjdc extends React.Component {
     render () {
         return (
             <div>
                 C页面
+                需要注意的是，如果你用MemoryRouter路由模式，Prompt不起作用。
+                <Prompt message="残忍离开？"></Prompt>
             </div>
         )
     }
