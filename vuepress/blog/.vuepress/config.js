26749604191e1bf53d43cb59405a0a48e2a8b68e
wu529778790 <529778790@qq.com>
2018-04-26 17:50:46 +0800

    增加base

--- vuepress/blog/.vuepress/config.js
+++ vuepress/blog/.vuepress/config.js
@@ -1,7 +1,11 @@
 module.exports = {
   title: '神族九帝',
   description: '永不言弃',
-  themeConfig: {},
+  dest: 'dist',
+  base: '/blog/',
+  themeConfig: {
+    repo: 'https://github.com/wu529778790/blog'
+  },
   configureWebpack: {
     // webpack别名设计
     resolve: {
