baab327caf26acc688fde2585b0fb0b4cd9ca416
wu529778790 <529778790@qq.com>
2018-05-08 18:00:52 +0800

    估计要放弃了，后面的太浅显了

---
+++ vue-koa2/src/components/pages/ShoppingMall.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="shoppingMall">
+    <!--search bar layout-->
+    <div class="search-bar">
+      <van-row gutter="5">
+        <van-col span="3"><img :src="locationIcon"
+            width="80%"
+            class="location-icon" /></van-col>
+        <van-col span="16">
+          <input type="text"
+            class="search-input" />
+        </van-col>
+        <van-col span="5">
+          <van-button size="mini">查找</van-button>
+        </van-col>
+
+      </van-row>
+    </div>
+    <!--swipwer area-->
+    <div class="swiper-area">
+      <van-swipe :autoplay="1000">
+        <van-swipe-item v-for="(banner,index) in bannerPicArray"
+          :key="index">
+          <img :src="banner.imageUrl"
+            width="100%" />
+        </van-swipe-item>
+      </van-swipe>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      locationIcon: require('../../assets/images/logo.png'),
+      bannerPicArray: [
+        {
+          imageUrl:
+            'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic001.jpg'
+        },
+        {
+          imageUrl:
+            'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic002.jpg'
+        },
+        {
+          imageUrl:
+            'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic003.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.search-bar {
+  height: 2.2rem;
+  background-color: #e5017d;
+  line-height: 2.2rem;
+}
+.search-input {
+  width: 100%;
+  height: 1.3rem;
+  border-top: 0px;
+  border-left: 0px;
+  border-right: 0px;
+  border-bottom: 1px solid 1px !important;
+  background-color: #e5017d;
+  color: #fff;
+}
+.location-icon {
+  padding-top: 0.2rem;
+  padding-left: 0.3rem;
+}
+.swiper-area {
+  width: 20rem;
+  clear: both;
+}
+</style>
--- vue-koa2/src/router/index.js
+++ vue-koa2/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import ShoppingMall from '@/components/ShoppingMall'
+import ShoppingMall from '@/components/pages/ShoppingMall'
 
 Vue.use(Router)
 
