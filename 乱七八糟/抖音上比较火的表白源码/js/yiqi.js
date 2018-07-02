11de88bced33d1578cf22732d0b2fbc9fede8d20
wu529778790 <529778790@qq.com>
2018-07-02 11:30:13 +0800

    抖音上很火的表白源码

---
+++ 乱七八糟/抖音上比较火的表白源码/js/yiqi.js
@@ -0,0 +1,74 @@
+// JavaScript Document
+var Dianji = 0;
+window.onload = function () {
+  var buhao = document.getElementById("buhao");
+  var hao = document.getElementById("hao");
+  buhao.onclick = function () {
+    Dianji++;
+    if (Dianji == 1) {
+      alert("小姐姐在考虑一下呗");
+    } else if (Dianji == 2) {
+      alert("你就在考虑一下吧");
+    } else if (Dianji == 3) {
+      alert("我会一生一世爱你");
+    } else if (Dianji == 4) {
+      alert("家务我全干");
+    } else if (Dianji == 5) {
+      alert("不藏私房钱");
+    } else if (Dianji == 6) {
+      alert("房子写你名");
+    } else if (Dianji == 7) {
+      alert("工资全上交");
+    } else if (Dianji == 8) {
+      alert("真的就不再考虑下");
+    } else if (Dianji == 9) {
+      alert("你出个价吧,我想把我卖了");
+    } else if (Dianji == 10) {
+      alert("什么，你还不同意");
+    } else if (Dianji == 11) {
+      alert("好吧，给你最后一次机会");
+    } else if (Dianji == 12) {
+      alert("真的最后一次");
+    } else if (Dianji == 13) {
+      alert("没有下次了");
+    } else if (Dianji == 14) {
+      document.getElementById('img').src = 'images/2.jpg'
+      alert("本宝宝生气了");
+    } else if (Dianji == 15) {
+      document.getElementById('img').src = 'images/3.jpg'
+      alert('不要怪我');
+    } else if (Dianji == 16) {
+      document.getElementById('img').src = 'images/4.jpg'
+      alert('宝宝心里委屈');
+    } else if (Dianji == 17) {
+      document.getElementById('img').src = 'images/5.jpg'
+      alert('但也是你逼我的');
+    } else if (Dianji == 18) {
+      document.getElementById('img').src = 'images/6.jpg'
+      alert('我爱你 路还长慢慢走 爱你一次 不怕最后会无疾而终 但不爱你一次 怕遗憾半生')
+      alert('我爱你 那样狂欢又美好的时光 像小时候偷喝了蜜糖 踩在云端 甜在心里 尽管一年只有一次 等了太久 可我还是愿意 愿意再等等你')
+      alert('我爱你 我还是相信 星星会说话 石头会开花 穿过夏天的木栅栏 和冬天的风雪之后 你终会抵达')
+      alert('我爱你 北上看到的冰川是你，南下追寻的极光是你，西去流浪的经幡是你，东去皈依的佛经也是你，世不遇你，生无可喜。')
+      alert('我爱你 他不过随手给了朵花 你却红了脸想用余生做代价')
+      alert('我爱你 拂你眉间哀愁 愿你百岁无忧 再见')
+      alert('我爱你 全身而退我不会，我等时间惭愧 ​​​​')
+      alert('我爱你 不要去追一匹马 用追马的时间种草 待到春暖花开 会有骏马任你挑选 不用刻意去接近谁 用暂时无人同行的时间 提升自己的能力 待到时机成熟的时候 自然会有一群人与你同行 再见')
+      alert('我爱你 故事不长也不难讲 恨过 吵过 打过 但一切都源于我们曾爱过 听说你后来混得风生水起 可我 还是喜欢当初那个一无所有的你 再见')
+      alert('我爱你 在我走之后，把我的骨灰带在身上，遇到坏人就扬出去，让我再保护你最后一次。。。')
+      setTimeout(() => {
+        window.location.href = "http://shenzjd.com"
+      }, 2000)
+    }
+  }
+  hao.onclick = function () {
+    alert("我爱你 假如可以，我想余生的每一秒记忆都是关于你");
+    alert('我爱你 想带你去看晴空万里 想要为你着迷 愿往后余生 风雪是你 平淡是你 目光所致 也是你 ​​​​');
+    alert('我爱你 想带你去极地看雪  想和你去青海湖环骑 愿每日三餐 炸鸡是你 烤鱼是你 酸甜苦辣 也是你');
+    alert('我爱你 从和你相遇的第一天起 你总说我从一开始就赢得了你的心。 你不知道的是， 在你认识我之前， 我就注意到了你， 小心翼翼， 步步为进， 那天街拐角的偶然相遇， 是我准备了许久的小确幸。')
+    alert('我爱你 我把我整个的灵魂都给你，连同它的怪癖，耍小脾气，忽明忽暗，一千八百种坏毛病。它真讨厌，只有一点好，爱你。')
+    alert('我爱你 等你时我觉得 一日不见如隔三秋，见你时，我又觉得三秋未见不过一日')
+    setTimeout(() => {
+      window.location.href = "http://shenzjd.com"
+    }, 2000)
+  }
+}
