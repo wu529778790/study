5b65538ed203a7317edcc48b0469f99dcbebfac2
wu529778790 <529778790@qq.com>
2018-07-27 17:43:51 +0800

    星期一继续

--- vue+Koa2+MpVue/serve/serve.js
+++ vue+Koa2+MpVue/serve/serve.js
@@ -1,8 +1,30 @@
 const Koa = require('koa')
 const app = new Koa()
 
-app.use(async ctx => {
-  ctx.body = 'hello koa'
+// app.use(async (ctx, next) => {
+//   ctx.body = 'hello koa'
+// })
+
+app.use(async (ctx, next) => {
+  ctx.body = '1'
+  setTimeout(() => {
+    next()
+  }, 1000)
+  ctx.body += '2'
+})
+app.use(async (ctx, next) => {
+  ctx.body += '3'
+  setTimeout(() => {
+    next()
+  }, 1000)
+  ctx.body += '4'
+})
+app.use(async (ctx, next) => {
+  ctx.body += '5'
+  setTimeout(() => {
+    next()
+  }, 1000)
+  ctx.body += '6'
 })
 
 app.listen(2000, () => {
